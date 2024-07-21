a1c657723a67b238ec9667d3057345717d2727ea
pavi <rkpavi06@gmail.com>
2024-07-21 20:37:13 +0530

    .

--- build/lib/geminikit/__init__.py
+++
@@ -1,2 +0,0 @@
-from geminikit.gemini import Gemini
-from geminikit.asynic_gemini import Gemini as Asynic_Gemini
--- build/lib/geminikit/helpers.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-def generate_random_string():
-    length = 1230
-    characters = string.ascii_letters + string.digits + '-_'
-    random_string = ''.join(secrets.choice(characters) for _ in range(length))
-    return "!"+random_string
