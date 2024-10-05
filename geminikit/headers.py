1c98534c00f720d5f38275a806562eb1cffccb44
pavi <rkpavi06@gmail.com>
2024-10-05 08:28:50 +0530

    fixed Internal Server Error

--- geminikit/headers.py
+++ geminikit/headers.py
@@ -1,5 +1,4 @@
 header_img_up = {
-
     "X-Same-Domain": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
@@ -36,27 +35,33 @@ header_img_int = {
 
 
 header_main = {
-    'authority': 'gemini.google.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-arch': '"x86"',
+    'cache-control': 'max-age=0',
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    'sec-ch-ua-arch': '""',
     'sec-ch-ua-bitness': '"64"',
-    'sec-ch-ua-full-version': '"115.0.5790.102"',
-    'sec-ch-ua-full-version-list': '"Not/A)Brand";v="99.0.0.0", "Google Chrome";v="115.0.5790.102", "Chromium";v="115.0.5790.102"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-ch-ua-platform-version': '"6.3.0"',
+    'sec-ch-ua-form-factors': '',
+    'sec-ch-ua-full-version': '"129.0.6668.58"',
+    'sec-ch-ua-full-version-list': '"Google Chrome";v="129.0.6668.58", "Not=A?Brand";v="8.0.0.0", "Chromium";v="129.0.6668.58"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Nexus 5"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"6.0"',
     'sec-ch-ua-wow64': '?0',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
+    'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',  
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36',
+    'x-browser-channel': 'stable',
+    'x-browser-copyright': 'Copyright 2024 Google LLC. All rights reserved.',
+    'x-browser-validation': 'vrdrOb79TZVKQF8a3V5ixRDvbQE=',
+    'x-browser-year': '2024',
+    'x-client-data': 'CIy2yQEIpLbJAQipncoBCLOKywEIkqHLAQiHoM0BCI2nzQEIwbbOAQj6wM4BCN3CzgEIy8TOAQiNxs4BCJLGzgEYnLHOARjDvc4B',
 }
-
-
 
 header_gen = {
     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
